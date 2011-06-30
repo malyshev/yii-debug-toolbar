19e8bc03f8e62ffe9de38ff809a7843466e3e759
Sergey Malyshev <lunga.web@gmail.com>
2011-06-30 23:55:24 +0300

    Tests

--- tests/unit/YiiDebugToolbarTest.php
+++ tests/unit/YiiDebugToolbarTest.php
@@ -5,11 +5,14 @@ class YiiDebugToolbarTest extends CTestCase
     public function testYiiDebugToolbarRoute()
     {
         $routeObject = new YiiDebugToolbarRoute();
-        $this->assertNull($routeObject->getStartTime());
-        $this->assertNull($routeObject->getEndTime());
-        $this->assertNull($routeObject->init());
-        $this->assertRegExp('/\d{1,}/', (string)$routeObject->getStartTime());
 
+        $this->assertNull($routeObject->getStartTime());
+
+        $this->assertNull($routeObject->getEndTime());
+
+        $this->assertNull($routeObject->init());
+        
+        $this->assertRegExp('/^\d{1,}$/', (string)$routeObject->getStartTime());
 
         $this->assertNotNull(self::getMethod('YiiDebugToolbarRoute','getToolbarWidget')
              ->invokeArgs($routeObject, array()));
@@ -21,11 +24,12 @@ class YiiDebugToolbarTest extends CTestCase
              ->invokeArgs($routeObject, array(new CEvent(Yii::app()))));
 
         $allowIpMethod = self::getMethod('YiiDebugToolbarRoute','allowIp');
+
         $this->assertEquals(true, $allowIpMethod->invokeArgs($routeObject, array('127.0.0.1')));
+
         $this->assertEquals(false, $allowIpMethod->invokeArgs($routeObject, array('127.0.0.2')));
 
         $this->assertNull($routeObject->collectLogs(new CLogger, false));
-
     }
 
 
