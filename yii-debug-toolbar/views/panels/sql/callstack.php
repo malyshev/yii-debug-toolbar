e251fa33794fcbf5288dd01f51c7398bedb23d37
Mads Nielsen <k4emic@gmail.com>
2011-08-19 23:21:18 +0200

    Line numbers for SQL callstack

--- yii-debug-toolbar/views/panels/sql/callstack.php
+++ yii-debug-toolbar/views/panels/sql/callstack.php
@@ -9,6 +9,7 @@
 <table id="yii-debug-toolbar-sql-callstack" class="tabscontent">
     <thead>
         <tr>
+        	<th>ID</th>
             <th>Query</th>
             <th nowrap="nowrap">Time (s)</th>
         </tr>
@@ -16,6 +17,7 @@
     <tbody>
         <?php foreach($callstack as $id=>$entry): ?>
         <tr class="<?php echo ($id%2?'odd':'even') ?>">
+        	<td class="text-right"><?php echo $id; ?></td>
             <td width="100%"><?php echo CHtml::encode($entry[0]); ?></td>
             <td nowrap="nowrap"><?php echo sprintf('%0.6f',$entry[1]); ?></td>
         </tr>
