afcb27ca748a554089df1eeb0aaea6c537d9fccc
Sergey Malyshev <malyshev.php@gmail.com>
2014-07-05 15:25:16 +0300

    Enhanced dump function

--- YiiDebug.php
+++ YiiDebug.php
@@ -24,10 +24,33 @@ class YiiDebug extends CComponent
      * but is more robust when handling complex objects such as Yii controllers.
      * @param mixed $var variable to be dumped
      */
-    public static function dump($var, $depth=10)
+    public static function dump()
     {
-        is_string($var) && $var = trim($var);
-        echo str_replace('&nbsp;', ' ', CVarDumper::dumpAsString($var, $depth, true));
+        $args = func_get_args();
+
+        if (php_sapi_name() == "cli") {
+            foreach ($args as $k => $var) {
+                var_dump($var);
+                echo "\n";
+            }
+            return;
+        } else if (empty($_SERVER['SERVER_ADDR']) || empty($_SERVER['REMOTE_ADDR']) || $_SERVER['SERVER_ADDR'] !== $_SERVER['REMOTE_ADDR']) {
+            return;
+        }
+
+        $backTrace = debug_backtrace();
+        $backTrace = array_shift($backTrace);
+        echo '<div style="margin: 10px;border: 1px solid red;padding: 10px; background: #fff;">';
+        if (is_array($backTrace) && isset($backTrace['file']) && isset($backTrace['function']) && $backTrace['function'] === __FUNCTION__) {
+            echo "<b>{$backTrace['file']}</b> in line <b>{$backTrace['line']}</b> <br />";
+            echo '<div style="border-bottom:1px solid #006699;margin: 5px 0;"></div>';
+        }
+
+        foreach ($args as $k => $var) {
+            echo CVarDumper::dump($var, 10, true), '<br />';
+        }
+
+        echo "</div>";
     }
 
     /**
